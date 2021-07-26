2ded0dd9522cc451a6f6413719321190571dee15
George <ggottemo717@gmail.com>
2021-07-26 14:36:46 -0400

    basic gui

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,4 +1,47 @@
-function computerPlay(){ //Randomly picks computer's choice
+const div = document.createElement('div');
+const CHOICES = ['ROCK', 'PAPER', 'SCISSORS'];
+
+var playerScore = 0;
+var computerScore = 0;
+
+for ( let i = 0; i < 3; i++){ //Adds buttons for each choice to DOM
+	let button = document.createElement('button');
+	button.textContent = CHOICES[i];
+	button.classList.add('choice-btn');
+	button.setAttribute('id', CHOICES[i].toLowerCase())
+	button.setAttribute('style', 'margin: auto; border: 1px solid black; margin-left: 3vw; cursor: pointer; width: 100px; height:30px; margin-top: 40%;')
+	button.addEventListener('click', () => console.log(game(button.getAttribute('id'), computerPlay())));
+	div.appendChild(button);
+}
+const body = document.querySelector('body');
+body.appendChild(div);
+
+var scoreDiv = document.createElement('div');
+scoreDiv.setAttribute('id', 'score');
+body.appendChild(scoreDiv);
+//Update Score 
+function updateScore(gameResult) {
+	switch(gameResult) {
+		case 'W':
+		playerScore++; break;
+		case 'L':
+		computerScore++; break;
+		case'Tie':
+		break;
+	}
+	let scoreDiv = document.querySelector('#score');
+	scoreDiv.setAttribute('style', 'border: 2px solid black; background-color: gray; margin-left: 60%; padding-left: 2vw;');
+	
+	
+	scoreDiv.textContent = `Player Score: '${window.playerScore} ' \n Computer Score: '${window.computerScore}'`;
+	
+	body.appendChild(scoreDiv);
+
+}
+
+
+ //Randomly picks computer's choice
+function computerPlay(){
 	let random = Math.floor(Math.random()* 3)
 
 	switch(random){
@@ -60,29 +103,18 @@ function playRound(playerChoice, computerChoice){
 }
 
 //game - plays 5 rounds and determines a winner.
-function game(){
-	let playerScore = 0;
-	let computerScore = 0;
-
-	for(let i = 0; i < 5; i++){
-		result = playRound(playerChoice(), computerPlay());
-		if( result  == 'W'){
-			playerScore++;
-			document.write('You won a round! \n');
-		}
-		else if (result == 'Tie'){
-			document.write('Tie');
-		}
-		else{
-			computerScore++;
-			document.write('You lost a round \n');
-		}
+function game(playerChoice, computerChoice){
+	updateScore(playRound(playerChoice, computerChoice));
+	victoryDiv = document.createElement('div');
+ 	
+	if (playerScore >= 5 && playerScore > computerScore){
+		victoryDiv.textContent = `You won with '${playerScore}' victories.`;
+		body.appendChild(victoryDiv);
+	}
+	if(computerScore >= 5 && computerScore > playerScore){
+		victoryDiv.textContent =`You lost with '${computerScore}' losses.`;
+		body.appendChild(victoryDiv);
 	}
 
-	if(playerScore > computerScore)
-		return 'You win!'
-	else
-		return 'You lost!'
 }
 
-document.write(game());
